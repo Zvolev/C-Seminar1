3e67287b2505774b4a6e877b0788afa0c248a711
Evgenii <zvolev@mail.ru>
2022-10-03 22:52:01 +0300

    Задача 2

---
+++ DZ2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+
+Console.Write("Введите первое число ");
+string inputStrihga = Console.ReadLine();
+int a = Convert.ToInt32(inputStrihga);
+
+Console.Write("Введите второе число ");
+string inputStrihgb = Console.ReadLine();
+int b = Convert.ToInt32(inputStrihgb);
+
+Console.Write("Введите третье число ");
+string inputStrihgc = Console.ReadLine();
+int c = Convert.ToInt32(inputStrihgb);
+
+int max = a;
+
+if (b>max) max = b;
+if (c>max) max = c;
+
+Console.Write($"Максимальное число {max}");
